3896f91f31fa2441cd82a0b019214255bdf47e18
Shany Golan <shany.golan@sap.com>
2019-01-27 21:01:43 +0200

    + removed old code

--- server/app/routes.py
+++ server/app/routes.py
@@ -74,12 +74,6 @@ def upload():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             result['data'] = inferAll()
 
-
-    #if request.method == 'GET':
-    #    inferAll()
-    #    return "done"
-
-
     resp = jsonify(result)
     resp.status_code = 200
     return resp
@@ -88,54 +82,4 @@ def upload():
 @app.route('/')
 @app.route('/index')
 def index():
-    html = """
-		<!doctype html>
-		<html lang="en">
-		  <head>
-			<!-- Required meta tags -->
-			<meta charset="utf-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-			<!-- Bootstrap CSS -->
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
-			<title>Hello, world!</title>
-		  </head>
-		  <body>
-			<nav class="navbar navbar-light bg-light">
-			  <a class="navbar-brand" href="#">
-				<img src="/static/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-				ShanyNet
-			  </a>
-			</nav>
-			<div>
-				<div class="container-fluid">
-				  <div class="row" style="margin-top:10px;">
-				  	<div class="col-5">
-					  <ul class="list-group">
-						  <li class="list-group-item">
-						  	<form action="/upload" method="post" enctype="multipart/form-data">
-						  		<input type="file" name="file"/>
-						  		<button type="submit" class="btn btn-primary">Upload</button>
-						  	</form>
-						  </li>
-						</ul>
-					</div>
-					<div class="col-7">
-					  right
-					</div>
-				  </div>
-				</div>
-			</div>
-			<!-- Optional JavaScript -->
-			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-			<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-			<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.6/angular.min.js"></script>
-		  </body>
-		</html>
-	"""
-
-    return html
+    return "nothing to see here"
