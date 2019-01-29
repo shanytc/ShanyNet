cc888feb69d1a64c976c92d2a7841b411022165c
Shany Golan <shany.golan@sap.com>
2019-01-29 22:33:26 +0200

    + update

--- server/app/routes.py
+++ server/app/routes.py
@@ -8,6 +8,7 @@ import numpy as np
 from time import time
 from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 from flask import jsonify
+from annoy import AnnoyIndex
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 NUM_OF_IMAGES_TO_SEARCH = 10
@@ -16,13 +17,36 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-filename = '/ib/junk/junk/shany_ds/shany_proj/server/100k.csv'
+filename = '/ib/junk/junk/shany_ds/shany_proj/server/inference.csv'
 print("Loading embeddings into memory...")
 start = time()
 data = pd.read_csv(filename, delimiter=',')
 
 print("Creating matrix...")
 embed = data.iloc[:, 3:515].as_matrix()
+
+# build annoy
+#t = AnnoyIndex(embed.shape[1])
+#i=0
+#print("adding elements to annoy...")
+#for element in embed:
+#    t.add_item(i,element)
+#    i+=1
+
+#print("Creating annoy tree...")
+#t.build(10) # 10 trees
+
+#print("saving annoy..")
+#t.save('inference.ann')
+
+#print('Annoy indexing complete...')
+
+# use annoy
+print('Loading annoy db...')
+u = AnnoyIndex(embed.shape[1])
+u.load('inference.ann')
+
+print("all done!")
 
 def inferAll():
     print("Inference crunching time...")
@@ -32,6 +56,15 @@ def inferAll():
     original_embed = res[0][2]
     ### scan embedding
 
+
+    u_res = u.get_nns_by_vector(original_embed, 10, -1, True)
+
+    res = []
+    for i,d in zip(u_res[0], u_res[1]):
+        res.append([float(d), int(data.iloc[i, 516]), data.iloc[i,0], int(data.iloc[i,515]) ])
+
+    return res
+    res = []
     similatirties = []
 
     print("finding nearest neighbhoors...")
