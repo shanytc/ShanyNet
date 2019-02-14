49e9d84d269e82fb444ea992ab8c3bcd34831fad
Shany Golan <shany.golan@sap.com>
2019-02-14 14:47:33 +0200

    + split test/train_val to functions

--- shanynet.py
+++ shanynet.py
@@ -6,8 +6,7 @@ from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import History
-from keras.optimizers import SGD
-from sklearn.metrics import classification_report
+from keras.optimizers import Adam
 from sklearn.preprocessing import LabelBinarizer
 import matplotlib.pyplot as plt
 import pickle
@@ -55,41 +54,41 @@ class ImageLoader(object):
 def ShanyNet2():
 	""" use of 6 conv layers, 1 fully connected """
 	model = Sequential()
-	model.add(Conv2D(32, (3, 3), padding='same', activation='relu',input_shape=(224,224,3))) # 32 filters, size of 3x3
+	model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(224, 224, 3)))  # 32 filters, size of 3x3
 	model.add(Conv2D(32, (3, 3), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.25))
 
-	model.add(Conv2D(64, (3, 3), padding='same', activation='relu')) # 64 filters, size 3x3
+	model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))  # 64 filters, size 3x3
 	model.add(Conv2D(64, (3, 3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2))) # 2x2 max pooling
-	model.add(Dropout(0.25))
+	model.add(MaxPooling2D(pool_size=(2, 2)))  # 2x2 max pooling
+	model.add(Dropout(0.25)) # technique used to tackle Overfitting
 
 	model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
 	model.add(Conv2D(64, (3, 3), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.25))
 
-	model.add(Flatten()) # flat
-	model.add(Dense(512, activation='relu'))
+	model.add(Flatten())  # flat
+	model.add(Dense(512, activation='relu', name="layer_1"))
 	model.add(Dropout(0.5))
-	model.add(Dense(100, activation='softmax')) # 100 classes
+	model.add(Dense(100, activation='softmax', name="layer_2"))  # 100 classes
 
+	model.summary()
 	return model
 
 
-def train_val_test(model=None, train_val_path='', test_path='', batch=16, epochs=100, classes=[], lbl=None):
+def train_val_test(model=None, train_val_path='', batch=16, epochs=100):
 	if model is None:
 		return
 
 	if train_val_path == '':
 		return
 
-	if test_path == '':
-		return
+	enable_multithreading = True
+	threads = 5
 
 	train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-	test_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 	valid_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 
 	train_generator = train_datagen.flow_from_directory(
@@ -104,14 +103,7 @@ def train_val_test(model=None, train_val_path='', test_path='', batch=16, epochs
 		batch_size=batch,
 		class_mode='binary')
 
-	test_generator = test_datagen.flow_from_directory(
-		directory=test_path,
-		target_size=(224, 224),
-		color_mode="rgb",
-		batch_size=batch,
-		class_mode=None,
-		shuffle=False
-	)
+
 
 	# calculate best steps for training
 	STEP_SIZE_TRAIN=train_generator.n/train_generator.batch_size
@@ -127,8 +119,8 @@ def train_val_test(model=None, train_val_path='', test_path='', batch=16, epochs
 		epochs=epochs,
 		validation_data=val_generator,
 		validation_steps=batch,
-		use_multiprocessing=True,
-		workers=6,
+		use_multiprocessing=enable_multithreading,
+		workers=threads,
 		callbacks=[history])
 
 	# save model and weights
@@ -146,12 +138,39 @@ def train_val_test(model=None, train_val_path='', test_path='', batch=16, epochs
 
 	# model.evaluate_generator(generator=val_generator, steps=STEP_SIZE_VALID, use_multiprocessing=True, workers=6)
 
+
+
+def test_model(model=None, test_path=''):
+	if model is None:
+		return
+
+	if test_path == '':
+		return
+
+	enable_multithreading = True
+	threads = 5
+
+	test_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+
+	test_generator = test_datagen.flow_from_directory(
+		directory=test_path,
+		target_size=(224, 224),
+		color_mode="rgb",
+		batch_size=batch,
+		class_mode=None,
+		shuffle=False
+	)
+
+	STEP_SIZE_TEST = test_generator.n/test_generator.batch_size
+
 	print("Testing network...")
 	test_generator.reset()
-	prediction=model.predict_generator(test_generator,steps=STEP_SIZE_VALID,use_multiprocessing=True, workers=6,verbose=1)
+	prediction=model.predict_generator(test_generator, steps=STEP_SIZE_TEST, use_multiprocessing=enable_multithreading, workers=threads, verbose=1)
 	predIdxs=np.argmax(prediction,axis=1)
 
 	print(prediction)
+
+	return predIdxs
 
 def load_model():
 	json_file = open('shanynet2.json', 'r')
@@ -197,20 +216,24 @@ def main():
 	# from tensorflow.python.client import device_lib
 	# print(device_lib.list_local_devices())
 
-	# conf
-	batch = 16
-	epochs = 100
-
 	#load model
 	#model = load_model()
 	#return
 
+	# conf
+	batch = 16
+	epochs = 10  # number of epochs
+
+
+
 	# train from new model
 	model = ShanyNet2()
-	#opt = 'rmsprop'
-	opt = SGD(lr=1e-2, momentum=0.9, decay=1e-2 / epochs)
+	# opt = 'rmsprop'
+	# opt = 'sgd'
+	# opt = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+	opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 	model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-	train_val_test(model=model, train_val_path=train_val_data_path, test_path=test_data_path, batch=batch, epochs=1, classes=classes, lbl=lb)
+	train_val_test(model=model, train_val_path=train_val_data_path, test_path=test_data_path, batch=batch, epochs=epochs)
 
 main()
